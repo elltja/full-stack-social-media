115245f63c1882bef531c22a618b858a189b2fa4
elltja <estq2009@gmail.com>
2025-05-15 07:54:22 +0200

    Improve middleware and api route

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,10 +13,15 @@ export default async function middleware(request: NextRequest) {
 }
 
 function isProtectedRoute(pathname: string) {
-  const protectedRoutePatterns = [
-    /^\/accounts\/profile$/,
-    /^\/$/,
-    /^\/post\/.*$/,
-  ];
-  return protectedRoutePatterns.some((pattern) => pattern.test(pathname));
+  if (pathname === "/accounts/signup" || pathname === "/accounts/signin") {
+    return false;
+  }
+  return true;
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
