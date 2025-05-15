b771ea7c2db61596e187a8b3c1622de9d41d98eb
elltja <estq2009@gmail.com>
2025-05-15 07:16:06 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,16 +5,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const parseFormData =
-  <T extends { inputs: Record<string, string> }>(
-    action: (data: T["inputs"]) => T | Promise<T>
-  ) =>
-  async (state: T, formData: unknown): Promise<T> => {
-    if (!(formData instanceof FormData)) {
-      return { ...state, error: "Invalid request" } as T;
-    }
-
-    const data = Object.fromEntries(formData.entries()) as T["inputs"];
-
-    return action(data);
-  };
